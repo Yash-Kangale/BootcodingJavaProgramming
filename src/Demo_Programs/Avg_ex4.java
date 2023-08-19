c92549d445b24243410f0ce79fc6f8d1625cd590
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-08-19 18:32:11 +0530

    Create Avg_ex4.java
    WAP to find avg of a number.

---
+++ src/Demo_Programs/Avg_ex4.java
@@ -0,0 +1,15 @@
+package Demo_Programs;
+
+public class Avg_ex4 {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=10;
+        int c=10;
+        float d=(a*b)/c;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println((a*b)/c);
+        System.out.println(d);
+    }
+}
