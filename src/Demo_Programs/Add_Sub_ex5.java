f1ffabaf13b63eed1ccaaa0d07acbe898e6cdad4
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-08-19 18:28:29 +0530

    Create Add_Sub_ex5.java
    WAP to print  add, sub, mul, div of a number.

---
+++ src/Demo_Programs/Add_Sub_ex5.java
@@ -0,0 +1,13 @@
+package Demo_Programs;
+
+public class Add_Sub_ex5 {
+    public static void main(String[] args)
+    {
+        int a=50;
+        int b=25;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
