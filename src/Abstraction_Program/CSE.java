7fb17e4d8c116c652678b4d8e0807986db968d45
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-25 17:49:12 +0530

    Create CSE.java

---
+++ src/Abstraction_Program/CSE.java
@@ -0,0 +1,10 @@
+package Abstraction_Program;
+
+public class CSE extends EnginnerigBranch_Ex1{
+    public void show(){
+        System.out.println("Welcome to the Computer Science Engineering branch");
+    }
+    public long fee(){
+        return 180000;
+    }
+}
