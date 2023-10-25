3a83b34d62e22b1e2331dbc8673720471d803709
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-25 17:50:07 +0530

    Create Hash2.java

---
+++ src/Collection_Program/Set/HashSet/Hash2.java
@@ -0,0 +1,11 @@
+package Collection_Program.Set.HashSet;
+import java.util.HashSet;
+public class Hash2 {
+    public static void main(String[] args) {
+        HashSet<String> hashSet=new HashSet<>();
+
+        hashSet.add("A");
+        hashSet.add("B");
+
+    }
+}
