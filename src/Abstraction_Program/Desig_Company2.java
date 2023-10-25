1bdcb24db8d1d08be6fbde8ecf0b106cade1a2f0
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-25 17:48:58 +0530

    Create Desig_Company2.java

---
+++ src/Abstraction_Program/Desig_Company2.java
@@ -0,0 +1,10 @@
+package Abstraction_Program;
+
+public class Desig_Company2 {
+    public void show(){
+        System.out.println("Chief Operatons Officer");
+    }
+    public long fee(){
+        return 180000;
+    }
+}
