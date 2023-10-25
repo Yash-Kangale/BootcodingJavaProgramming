b858e463d13821092716a7981d047741d3aff076
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-25 17:48:33 +0530

    Create IT.java

---
+++ src/Abstraction_Program/IT.java
@@ -0,0 +1,12 @@
+package Abstraction_Program;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class IT extends EnginnerigBranch_Ex1{
+    public void show(){
+        System.out.println("Welcome to the Information Technology Engineering branch");
+    }
+    public long fee(){
+        return 150000;
+    }
+}
