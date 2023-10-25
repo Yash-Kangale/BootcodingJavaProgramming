882e702dfb1f7aa9b4da0fa1d1de508b0541cab6
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-25 17:46:07 +0530

    Create Linked.java

---
+++ src/Collection_Program/Set/LinkedSet/Linked.java
@@ -0,0 +1,19 @@
+package Collection_Program.Set.LinkedSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class Linked {
+    public static void main(String[] args) {
+        LinkedList<Object> linkedList=new LinkedList<>();
+
+        //add
+        linkedList.add(45);
+        linkedList.add(48);
+        linkedList.add(48);
+        linkedList.add(46);
+        linkedList.add(50);
+        System.out.println(linkedList);
+
+
+    }
+}
