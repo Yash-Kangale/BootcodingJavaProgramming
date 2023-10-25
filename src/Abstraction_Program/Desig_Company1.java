7e35fae7d4ba743272873250f372b24d031c9c9d
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-25 17:51:18 +0530

    Create Desig_Company1.java

---
+++ src/Abstraction_Program/Desig_Company1.java
@@ -0,0 +1,10 @@
+package Abstraction_Program;
+
+public class Desig_Company1 {
+    public void show(){
+        System.out.println("Chief Executing Officer");
+    }
+    public long fee(){
+        return 150000;
+    }
+}
