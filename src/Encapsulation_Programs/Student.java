f9fc7048c0e8a15ffcee743607353e1c4e1ed94f
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-25 17:47:54 +0530

    Create Student.java

---
+++ src/Encapsulation_Programs/Student.java
@@ -0,0 +1,31 @@
+package Encapsulation_Programs;
+
+public class Student {
+    int age;
+    String name;
+    String branch;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+}
