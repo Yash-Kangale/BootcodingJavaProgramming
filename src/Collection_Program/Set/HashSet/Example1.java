0e9fe04b34526aa80ecbd4ddb8241082463c4642
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-27 22:37:57 +0530

    Create Example1.java

---
+++ src/Collection_Program/Set/HashSet/Example1.java
@@ -0,0 +1,23 @@
+package Collection_Program.Set.HashSet;
+
+import java.util.HashSet;
+public class Example1 {
+    public static void main(String[] args) {
+        HashSet<Integer> hashSet=new HashSet<>();
+
+        //add
+        hashSet.add(12);
+        hashSet.add(15);
+        hashSet.add(20);
+        hashSet.add(50);
+        hashSet.add(45);
+        System.out.println(hashSet);
+
+        //remove
+        hashSet.remove(12);
+        System.out.println(hashSet);
+
+        System.out.println(hashSet.add(49));
+        System.out.println(hashSet);
+    }
+}
