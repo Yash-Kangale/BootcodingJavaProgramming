027c2e088a6767743e378b4260fa18eadedaef88
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-10-28 16:55:43 +0530

    Create Patient1.java

---
+++ src/Collection_Program/List/LinkedList/Assignment/Patient1.java
@@ -0,0 +1,26 @@
+package Collection_Program.List.LinkedList.Assignment;
+
+public class Patient1 {
+    private String name;
+    private int age;
+    private String bloodgr;
+    private String  disease;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age + '\'' +
+                ", bloodgr="+ bloodgr +'\'' +
+                ", disease="+'\'' +
+                '}';
+    }
+
+    public Patient1(String name, int age,String bloodgr,String disease) {
+        this.name = name;
+        this.age = age;
+        this.bloodgr=bloodgr;
+        this.disease=disease;
+    }
+}
+
