6e9756181a7f9d278623023dd4dfb92943b7c518
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-08 08:44:25 +0530

    Create Ex1.java

---
+++ src/Practice/If_else_programs/Ex1.java
@@ -0,0 +1,11 @@
+package Practice.If_else_programs;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        if(a<b)
+            System.out.println("b is greater than a");
+            else
+            System.out.println("a is greater than b");
+    }
+}
