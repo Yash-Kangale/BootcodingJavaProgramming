ebd838b321e423632bb23b1e9e9a268cf4959a8f
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-14 16:32:33 +0530

    Create Ex1.java

---
+++ src/Practice/Nested_If_else/Ex1.java
@@ -0,0 +1,16 @@
+package Practice.Nested_If_else;
+// Find the greatest no. among the three no.
+public class Ex1 {
+    public static void main(String[] args) {
+        int x=5,y=2,z=8;
+        if(x > y) {
+            if(x>z)
+                System.out.println("x is greatest integer");
+            else
+                System.out.println("x is not greatest integer");
+        }
+        else
+            System.out.println("x is not the greatest integer");
+        }
+    }
+
