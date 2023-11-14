683b727c0e7838405c22985f2c12e7e66f8db4af
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-14 16:32:45 +0530

    Create Ex2.java

---
+++ src/Practice/Nested_If_else/Ex2.java
@@ -0,0 +1,12 @@
+package Practice.Nested_If_else;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int x=5,y=2,z=8;
+        if((x>y) && (x>z)){
+            System.out.println("x is the greatest integer");
+        }
+        else
+            System.out.println("x is not the greatest integer");
+    }
+}
