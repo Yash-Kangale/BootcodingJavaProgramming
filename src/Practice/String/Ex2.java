a783c0b5814721bcda42faea1cf9116ddd90b92c
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-15 14:16:29 +0530

    Create Ex2.java

---
+++ src/Practice/String/Ex2.java
@@ -0,0 +1,15 @@
+package Practice.String;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        String s;
+        Scanner s1= new Scanner(System.in);
+
+        System.out.println("Enter the 1st name:");
+        s=s1.nextLine();
+
+        System.out.println("Your name is"+s);
+    }
+}
