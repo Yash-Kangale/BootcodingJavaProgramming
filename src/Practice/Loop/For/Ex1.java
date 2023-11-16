465657b18cd223da0cccbfec5389a2b08795c158
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-16 17:54:39 +0530

    Create Ex1.java

---
+++ src/Practice/Loop/For/Ex1.java
@@ -0,0 +1,11 @@
+package Practice.Loop.For;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int n;
+        for(n=1;n<=50;n++){
+            System.out.println(n);
+        }
+
+    }
+}
