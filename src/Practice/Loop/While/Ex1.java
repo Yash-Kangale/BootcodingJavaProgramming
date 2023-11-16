a09dde8cca199c32dad628a70c49e04b56f3c81c
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-16 17:55:31 +0530

    Create Ex1.java

---
+++ src/Practice/Loop/While/Ex1.java
@@ -0,0 +1,11 @@
+package Practice.Loop.While;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=50){
+            System.out.println(n);
+            n++;
+        }
+    }
+}
