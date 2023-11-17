8ca49ebaf9ed8452da4e13217142272f0b838d5c
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-17 16:38:39 +0530

    Create Ex3.java

---
+++ src/Practice/Loop/For/Ex3.java
@@ -0,0 +1,10 @@
+package Practice.Loop.For;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        int n=1;
+        for(;n<=10;n++){
+            System.out.println(n);
+        }
+    }
+}
