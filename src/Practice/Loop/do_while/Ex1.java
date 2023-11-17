c3cae30ae3b28409ad7ddaf7d3f858585a7690fe
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-17 16:37:57 +0530

    Create Ex1.java

---
+++ src/Practice/Loop/do_while/Ex1.java
@@ -0,0 +1,13 @@
+package Practice.Loop.do_while;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int n=1;
+        do {
+            System.out.println(n);
+            n++;
+        }
+        while(n<=50);
+    }
+}
+
