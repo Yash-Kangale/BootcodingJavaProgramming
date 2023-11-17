ec88a1b71384afc351bad60be662f5ca9688451b
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-17 16:38:52 +0530

    Create Ex4.java

---
+++ src/Practice/Loop/For/Ex4.java
@@ -0,0 +1,11 @@
+package Practice.Loop.For;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        int n;
+        for (n=1;n<=50;){
+            System.out.println(n);
+            n++;
+        }
+    }
+}
