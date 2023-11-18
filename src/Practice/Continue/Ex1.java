5015b53553857cd7790adc6f1a267347014c9019
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-18 13:34:12 +0530

    Create Ex1.java

---
+++ src/Practice/Continue/Ex1.java
@@ -0,0 +1,16 @@
+package Practice.Continue;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int n=1;
+        do{
+            if(n==5){
+                n=n+1;
+                continue;
+            }
+            System.out.println("n="+n);
+            n++;
+        }
+        while(n<10);
+    }
+}
