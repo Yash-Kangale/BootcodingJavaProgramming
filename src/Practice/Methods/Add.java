f9e3bd4e90a43ae7f324ce26a7376ad186ec025d
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-19 09:30:37 +0530

    Create Add.java

---
+++ src/Practice/Methods/Add.java
@@ -0,0 +1,14 @@
+package Practice.Methods;
+// Get the values from method
+// Print addition of two numbers //
+public class Add {
+    public static int add_int(int x,int y){
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        int z;
+        z=add_int(2,4);
+        System.out.println("Addition is = "+z);
+    }
+}
