95dd854deeb5740c163f945c52bc63619027d0cd
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-11-25 20:23:01 +0530

    Create Ex2.java

---
+++ src/Practice/Array/TwoD_Arrays/Ex2.java
@@ -0,0 +1,27 @@
+package Practice.Array.TwoD_Arrays;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int[][] s=new int[2][4];
+
+        s[0][0]=2;
+        s[0][1]=5;
+        s[0][2]=7;
+        s[0][3]=4;
+        s[1][0]=9;
+        s[1][1]=3;
+        s[1][2]=2;
+        s[1][3]=8;
+        System.out.println("Sum of the items produced in the 1st factory:");
+        int sum1=0,sum2=0;
+        for(int i=0;i<4;i++){
+            sum1+=s[0][i];
+        }
+        System.out.println(sum1);
+        System.out.println("Sum of the items produced in the second factory:");
+        for(int j=0;j<4;j++){
+            sum2+=s[1][j];
+        }
+        System.out.println(sum2);
+    }
+}
