8b36c1fcc14d1b010f27d8b30b3af8125a3a56c9
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-02 20:26:20 +0530

    Create Ex4.java

---
+++ src/Practice/String/ClassMethods/Ex4.java
@@ -0,0 +1,10 @@
+package Practice.String.ClassMethods;
+//boolean isEmpty()
+public class Ex4 {
+    public static void main(String[] args) {
+        String s1="Umbrella";
+        String s2="";
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
