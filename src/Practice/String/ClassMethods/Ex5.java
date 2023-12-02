9db873ed7de3816d63f1965bc5fc8175cc5bacab
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-02 20:26:38 +0530

    Create Ex5.java

---
+++ src/Practice/String/ClassMethods/Ex5.java
@@ -0,0 +1,10 @@
+package Practice.String.ClassMethods;
+// String
+public class Ex5 {
+    public static void main(String[] args) {
+        String s1="Nag";
+        s1=s1.concat("pur");
+        System.out.println(s1);
+    }
+
+}
