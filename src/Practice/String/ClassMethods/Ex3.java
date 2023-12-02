1efce715c47f9c1c7e9b49ddeedbdefab6e6337f
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-02 20:26:02 +0530

    Create Ex3.java

---
+++ src/Practice/String/ClassMethods/Ex3.java
@@ -0,0 +1,11 @@
+package Practice.String.ClassMethods;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        String s1="Umbrella";
+        String s2=s1.substring(2);
+        String s3=s1.substring(2,6);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
