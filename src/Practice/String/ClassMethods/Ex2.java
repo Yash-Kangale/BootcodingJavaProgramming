6667748dc8746e611b1d1a6c4422128f2387a9ee
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-02 20:25:49 +0530

    Create Ex2.java

---
+++ src/Practice/String/ClassMethods/Ex2.java
@@ -0,0 +1,9 @@
+package Practice.String.ClassMethods;
+// Find string length using "Length" keyword
+public class Ex2 {
+    public static void main(String[] args) {
+        String s1="Start Java Coding";
+        int l=s1.length();
+        System.out.println("String length is"+l);
+    }
+}
