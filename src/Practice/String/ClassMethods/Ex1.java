5bd4c53fa048ea64f467e54685d8a26d7dbd533c
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-02 20:25:21 +0530

    Create Ex1.java

---
+++ src/Practice/String/ClassMethods/Ex1.java
@@ -0,0 +1,9 @@
+package Practice.String.ClassMethods;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        String s1="Hello";
+        char ch=s1.charAt(3);
+        System.out.println(ch);
+    }
+}
