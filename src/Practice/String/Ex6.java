9674513dd43115cfb14edc6e85ee29c6750812cc
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-03 17:37:16 +0530

    Create Ex6.java

---
+++ src/Practice/String/Ex6.java
@@ -0,0 +1,9 @@
+package Practice.String;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        String s1="Switch on the Button";
+        String s2=s1.replaceAll("on","ui");
+        System.out.println(s2);
+    }
+}
