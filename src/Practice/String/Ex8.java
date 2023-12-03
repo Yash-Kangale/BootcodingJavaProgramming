2a72cc81f48860c4481242f1ca8ca3d0f4e91043
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-03 17:37:40 +0530

    Create Ex8.java

---
+++ src/Practice/String/Ex8.java
@@ -0,0 +1,11 @@
+package Practice.String;
+// String boolean contains
+public class Ex8 {
+    public static void main(String[] args) {
+        String s1="Samosa, AlooBonda, Kachori, Meduvada, Idli, Dosa ";
+        System.out.println(s1.contains("Samosa"));
+        System.out.println(s1.contains("Manchurian"));
+        System.out.println(s1.contains("Gulab Jamun"));
+        System.out.println(s1.contains("Kachori"));
+    }
+}
