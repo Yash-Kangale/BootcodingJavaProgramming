ba5c6eb19d95d865516a60f77b5c2e1d0c8fa151
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-03 17:37:28 +0530

    Create Ex7.java

---
+++ src/Practice/String/Ex7.java
@@ -0,0 +1,12 @@
+package Practice.String;
+// String to Lowercase and String to Uppercase
+public class Ex7 {
+    public static void main(String[] args) {
+        String s1="Welcome to the codespace";
+        String s2=s1.toLowerCase();
+        String s3=s1.toUpperCase();
+        System.out.println("String in Lowercase: " +s2);
+        System.out.println("String in Uppercase: " +s3);
+
+    }
+}
