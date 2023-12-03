2d56a4fa758f0ca9f4d697f0b47234c4afa812ee
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-03 17:36:49 +0530

    Create Ex5.java

---
+++ src/Practice/String/Ex5.java
@@ -0,0 +1,9 @@
+package Practice.String;
+// String Replace
+public class Ex5 {
+    public static void main(String[] args) {
+        String s1="Hello Nagpur";
+        String re=s1.replace('e','a');
+        System.out.println(re);
+    }
+}
