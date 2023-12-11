c7bfc84be3a187b7460ae13381d9b3db33b6161f
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-11 21:04:00 +0530

    Create RightTriangle.java

---
+++ src/Practice/Pattern/RightTriangle.java
@@ -0,0 +1,15 @@
+package Practice.Pattern;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        int rows = 10;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
