94682b0bcd289d42e86285d91e1fe32dee6470af
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-22 18:00:30 +0530

    Create User.java

---
+++ src/PROJECT/User.java
@@ -0,0 +1,46 @@
+package PROJECT;
+
+public class User {
+    private String id;
+    private String name;
+    private String address;
+    private String phone;
+
+    public User(String name, String id, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
