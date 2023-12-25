42425e4dc785f24f82dbb64270b830e4078a9453
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-25 13:28:28 +0530

    Create Ex1.java

---
+++ src/Method_Programs/Mathmatical/Ex1.java
@@ -0,0 +1,32 @@
+package Method_Programs.Mathmatical;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int m=Math.abs(-19);
+        System.out.println(m);
+
+        int max=Math.max(45,69);
+        int min=Math.min(95,65);
+        System.out.println(max);
+        System.out.println(min);
+
+        System.out.println(Math.pow(4,2));
+        double p=Math.pow(2,3);
+        System.out.println(p);
+
+        System.out.println(Math.round(45));
+        System.out.println(Math.round(95.00));
+
+        double s=Math.sqrt(64);
+        System.out.println(s);
+
+        double sq=Math.sqrt(81);
+        System.out.println(sq);
+        System.out.println(Math.sqrt(25));
+
+        double random=Math.random()*100;
+        double num=Math.round(random);
+        System.out.println(random);
+        System.out.println(num  );
+    }
+}
