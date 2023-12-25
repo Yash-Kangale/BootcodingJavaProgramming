014e27a6a736a5e0a1ae838fd74ae6b43650c08e
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2023-12-25 13:29:21 +0530

    Create Ex2.java

---
+++ src/Method_Programs/Mathmatical/Ex2.java
@@ -0,0 +1,44 @@
+package Method_Programs.Mathmatical;
+// Assignment Problem:
+public class Ex2 {
+    public static void main(String[] args) {
+
+        // Absolute Value- abs();
+        int a=Math.abs(-99);
+        System.out.println(a+"\n");
+
+        // Ceil Value- ceil();
+        double c=56.66;
+        System.out.println(Math.ceil(c)+ "\n");
+
+        // Round- round();
+        double r=45.95;
+        System.out.println(r+"\n");
+
+        // Floor- floor();
+        double f=95.05;
+        System.out.println(f+"\n");
+
+        // Max Value- max();
+        System.out.println(Math.max(65,95)+"\n");
+
+        // Min Value- min();
+        System.out.println(Math.min(98,1000)+"\n");
+
+        // Power- pow();
+        System.out.println(Math.pow(2,5)+"\n");
+
+        //  Random- random();
+        double random=Math.random()*100;
+        double ra=Math.round(r);
+        System.out.println(random+);
+        System.out.println(ra);
+
+        // Squareroot- sqrt();
+        double sq=225;
+        System.out.println(Math.sqrt(sq));
+
+
+
+    }
+}
