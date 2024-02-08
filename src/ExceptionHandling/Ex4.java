b983a939b74bd978b85c70178a84f662919ae4d7
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2024-02-08 18:10:16 +0530

    Create Ex4.java

---
+++ src/ExceptionHandling/Ex4.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        try{
+            int d1=100/2;
+            System.out.println(d1);
+        }catch(Exception e){
+            System.out.println("Invalid code");
+        }
+    }
+}
