9ce945923cc903f7dc9d961a23258609089d7590
Yash-Kangale <142515937+Yash-Kangale@users.noreply.github.com>
2024-02-08 18:10:07 +0530

    Create Ex5.java

---
+++ src/ExceptionHandling/Ex5.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        try{
+            System.out.println();
+        }catch(ArithmeticException e){
+            System.out.println("None");
+        }
+    }
+}
